c8a09c04735b0d30013600af749cf05184798093
Shlumpus <140124184+Shlumpus@users.noreply.github.com>
2024-11-20 10:39:43 +1100

    initialised a new workspace for assessment one and created my book class in the models file for my book database.

--- models.py
+++ models.py
@@ -2,5 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String, nullable=False)
+    
+
 # Define your database model here
 # Example: class Item(db.Model):
