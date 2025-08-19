5b6a9af60403c0db05d74f461287d8f278d463a5
Shlumpus <kaelan.taeni1@education.nsw.gov.au>
2025-08-19 14:57:26 +1000

    Replacing certain terms that had been in the previous base project for this pwa into terms for the beatstore being created, such as changing the Book class to Track, and changing it's specifications

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, flash
-from models import db,Book # Also import your database model here
+from models import db,Track # Also import your database model here
 
 # Define your routes inside the 'init_routes' function
 # Feel free to rename the routes and functions as you see fit
@@ -12,23 +12,23 @@ def init_routes(app):
     @app.route('/', methods=['GET'])
     def index():
         # This route should retrieve all items from the database and display them on the page.
-        books = Book.query.all()
-        return render_template('index.html', books=books)
+        tracks = track.query.all()
+        return render_template('index.html', tracks=tracks)
 
     @app.route('/add', methods=['GET', 'POST'])
     def add_item():
         if request.method == 'POST':
-            new_book = Book(
+            new_track = track(
                 title=request.form['title'],
-                author=request.form['author'],
-                year=int(request.form['year']),
+                producer=request.form['producer'],
+                date=int(request.form['date']),
                 rating=float(request.form['rating']),
                 genre=request.form['genre'],
                 description=request.form['description'],
                 image=request.form['image']
             )
 
-            db.session.add(new_book)
+            db.session.add(new_track)
             db.session.commit()
             return redirect(url_for('index'))
 
@@ -40,35 +40,35 @@ def init_routes(app):
         # This route should handle updating an existing item identified by the given ID.
         if request.method == 'POST':
             id = request.form['id']
-            book=Book.query.get_or_404(id)
-            book.title=request.form['title']
-            book.author=request.form['author']
-            book.year=int(request.form['year'])
-            book.rating=float(request.form['rating'])
-            book.genre=request.form['genre']
-            book.description=request.form['description']
-            book.image=request.form['image']
+            track=Track.query.get_or_404(id)
+            track.title=request.form['title']
+            track.producer=request.form['producer']
+            track.date=int(request.form['date'])
+            track.rating=float(request.form['rating'])
+            track.genre=request.form['genre']
+            track.description=request.form['description']
+            track.image=request.form['image']
             db.session.commit()
             return redirect(url_for('edit_item'))
         else:        
             id = request.args.get('id')
-            book=Book.query.get_or_404(id)
-            return render_template('edit_item.html', book=book)
+            track=Track.query.get_or_404(id)
+            return render_template('edit_item.html', track=Track)
 
     @app.route('/info', methods=['GET', 'POST'])
     def display_info():
         if request.method == 'GET':
             id=request.args.get('id')
-            book = Book.query.get(id)
-            return render_template('info.html', book=book)
+            track = Track.query.get(id)
+            return render_template('info.html', track=Track)
         else:
             return render_template('add_item.html')
 
     @app.route('/delete', methods=['GET'])
     def delete_item():
         id=request.args.get('id')
-        book = Book.query.get(id)
-        db.session.delete(book)
+        track = Track.query.get(id)
+        db.session.delete(track)
         db.session.commit()
         
         # This route should handle deleting an existing item identified by the given ID.
